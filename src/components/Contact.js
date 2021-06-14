c7984500a6c7fd06549ee21c50727e1af51bae23
Kevin Blair <kblair40@gmail.com>
2021-06-14 07:49:54 -0500

    Fix Resume header links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,43 +1,50 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
+import IconButton from "@material-ui/core/IconButton";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 const styles = {
   header: {
-    textAlign: "right",
     margin: 0,
   },
-  contactInfo: {
-    marginTop: ".5rem",
+  headerContainer: {
+    width: "20%",
+  },
+  headerContact: {
     display: "flex",
-    justifyContent: "space-evenly",
-    fontSize: ".8rem",
-    flexWrap: "wrap",
+    flexDirection: "column",
+    margin: ".5rem 0",
+  },
+  profileButtons: {
+    display: "flex",
   },
 };
 
 const Contact = ({ classes }) => {
   return (
-    <React.Fragment>
-      <h1 className={classes.header}>First Last</h1>
-      {/* <h5 className={classes.header}>Chicago, IL 60614</h5> */}
-      <h5 className={classes.header}>Location</h5>
-      <div className={classes.contactInfo}>
-        {/* <span>847-845-4635</span> */}
-        <span>888-888-8888</span>
-        {/* <span>kblair40@gmail.com</span> */}
-        <span>email@email.com</span>
-        <span>
-          <a target="blank" href="https://linkedin.com/in/kevin-blair-74525935">
-            linkedin
-          </a>
-        </span>
-        <span>
-          <a target="blank" href="https://www.github.com/kblair40">
-            github
-          </a>
-        </span>
+    <div className={classes.headerContainer}>
+      <h1 className={classes.header}>Kevin Blair</h1>
+      <h5 className={classes.header}>Chicago, IL 60614</h5>
+      <div className={classes.headerContact}>
+        <span>847-845-4635</span>
+        <span>kblair40@gmail.com</span>
       </div>
-    </React.Fragment>
+      <div className={classes.profileButtons}>
+        <IconButton
+          onClick={() => window.open("https://www.github.com/kblair40")}
+        >
+          <GitHubIcon />
+        </IconButton>
+        <IconButton
+          onClick={() =>
+            window.open("https://www.linkedin.com/in/kevin-blair-74525935")
+          }
+        >
+          <LinkedInIcon />
+        </IconButton>
+      </div>
+    </div>
   );
 };
 
